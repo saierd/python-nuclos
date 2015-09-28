702ea14cecc357d1f40e0619ba2e1f274b8e4b13
Daniel Saier <mail@danielsaier.de>
2015-09-28 12:04:26 +0200

    Bump to version 1.3

--- nuclos.py
+++ nuclos.py
@@ -3,7 +3,7 @@ Copyright (c) 2014-2015 Daniel Saier
 
 This project is licensed under the terms of the MIT license. See the LICENSE file.
 """
-__version__ = "1.2"
+__version__ = "1.3"
 
 import sys
 
